1e663191432c71fb116afd11cf26ecab555e9ec0
muriloleall07 <lealmurilo579@gmail.com>
2024-08-16 10:13:26 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    background("white");
+  }
+  
+  function draw() {
+    
+    stroke("light grey");
+    fill("purple");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
